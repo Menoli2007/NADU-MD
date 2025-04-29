1d1246b67f12f68e3dfd018a7a45becd576bddbd
NADU-v <Mybotmd1@gmail.com>
2025-04-29 09:45:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "ඇඩ් කරන්න ඔනේ",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/ZzWrWFLV/8520.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -43,7 +43,7 @@ OWNER_NAME: process.env.OWNER_NAME || "NADU-MD",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ NADU-MD*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "ඇඩ් කරන්න ඔනේ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/SDC0Z0Kq/3489.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> ᴘᴏᴡᴇʀᴇᴅ ʙʏ *NADU-MD*⚡",
 // add alive msg here 
